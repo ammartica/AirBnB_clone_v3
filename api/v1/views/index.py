02ef6d718fea39636674506ce9ed3fcd376f4217
ammartica <amisadaymartinez@gmail.com>
2022-05-21 18:05:01 +0000

    added enpoint that retrieves total number of obj by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,15 @@ from api.v1.views import app_views
 def objStatus():
     """status of obj app_views returned as JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """endpoint retrieves number of each objects by type"""
+    cls_dict = {'amenities': storage.count('Amenity'),
+                'cities': storage.count('City'),
+                'places': storage.count('Place'),
+                'reviews': storage.count('Review'),
+                'states': storage.count('State'),
+                'users': storage.count('User')}
+    return jsonify(cls_dict)
