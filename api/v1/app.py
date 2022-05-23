6ea50b5c1ad6f1b76b580884329d18d47b790647
ammartica <amisadaymartinez@gmail.com>
2022-05-23 19:29:12 +0000

    added documentation, Blueprint module, and contained CORS in variable cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,28 +4,29 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
-    """teardown_appcontext"""
+    """closes storage"""
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(code):
-    return jsonify({
+    """handler for error 404"""
+    return (jsonify({
         "error": "Not found"
-    }), 404
+    }), 404)
 
 
 if __name__ == "__main__":
-    PORT = getenv("HBNB_API_PORT", '8080')
+    PORT = getenv("HBNB_API_PORT", '5000')
     HOST = getenv("HBNB_API_HOST", '0.0.0.0')
     app.run(host=HOST, port=PORT, threaded=True)
