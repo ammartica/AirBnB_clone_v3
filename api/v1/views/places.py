b8ea62320d3c994accd80337cf866175e2e2e8ae
ammartica <amisadaymartinez@gmail.com>
2022-05-24 23:35:46 +0000

    add file

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-""" Handles all default RESTFul API """
-from flask import request, abort, jsonify
-from models import storage
-from models.state import State
-from models.city import City
+"""
+Places
+"""
+
 from api.v1.views import app_views
-from api.v1.app import *
+from flask import Flask, jsonify, abort, request
+from models import storage
+from models.place import Place
 
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['GET'], strict_slashes=False)
-def all_places(city_id):
-    """method retieves all places"""
-    city = storage.get(City, city_id)
-    if city is None:
+def get_places_by_city(city_id):
+    """method that retrieves a list of all places"""
+    my_city = storage.get('City', city_id)
+    if my_city is None:
         abort(404)
-    places = []
-    for place in city.places:
-        places.append(place.to_dict())
-    return jsonify(places)
+    places = my_city.places
+    places_list = []
+    for place in places:
+        places_list.append(place.to_dict())
+    return jsonify(places_list)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-def one_place(place_id):
-    """method that retrieves a place by id"""
-    place = storage.get(Place, place_id)
-    if place is not None:
+def get_places_by_id(place_id):
+    """method that retrieves a place filter by id"""
+    my_place = storage.get('Place', place_id)
+    if my_place is not None:
         return jsonify(my_place.to_dict())
     else:
         abort(404)
@@ -33,33 +35,33 @@ def one_place(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_place(place_id):
+def delete_places_by_id(place_id):
     """method that deletes a place by id"""
-    place = storage.get(Place, place_id)
-    if place is None:
+    delete_place = storage.get('Place', place_id)
+    if delete_place is None:
         abort(404)
-    storage.delete(place)
+    storage.delete(delete_place)
     storage.save()
     return jsonify({}), 200
 
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['POST'], strict_slashes=False)
-def create_place(city_id):
-    """method to create a new place"""
-    place = request.get_json()
-    if not place:
+def post_place(city_id):
+    """method to post a new place"""
+    new_place = request.get_json()
+    if not new_place:
         abort(400, 'Not a JSON')
-    if 'user_id' not in place:
+    if 'user_id' not in new_place:
         abort(400, 'Missing user_id')
-    if 'name' not in place:
+    if 'name' not in new_place:
         abort(400, 'Missing name')
-    city = storage.get(City, city_id)
-    if city is None:
+    my_city = storage.get('City', city_id)
+    if my_city is None:
         abort(404)
 
-    user = storage.get(User, request.json['user_id'])
-    if user is None:
+    my_user = storage.get('User', request.json['user_id'])
+    if my_user is None:
         abort(404)
 
     new_place = Place(name=request.json['name'],
@@ -70,18 +72,19 @@ def create_place(city_id):
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def update_place(place_id):
-    """method to update a place by id"""
-    place = storage.get(Place, place_id)
-    if place is None:
+def put_place(place_id):
+    """method to update/put a place by id"""
+    mod_place = storage.get('Place', place_id)
+    if mod_place is None:
         abort(404)
-    request = request.get_json()
-    if not request:
+    if not request.json:
         abort(400, 'Not a JSON')
-    request['id'] = place.id
-    request['user_id'] = place.user_id
-    request['city_id'] = place.city_id
-    request['created_at'] = place.created_at
-    place.__init__(**request)
-    place.save()
-    return jsonify(place.to_dict()), 200
+    req_place = request.get_json()
+    for key in req_place:
+        if key == 'id' or key == 'user_id' or\
+           key == 'city_id' or key == 'created_at' or key == 'updated_at':
+            pass
+        else:
+            setattr(mod_place, key, req_place[key])
+    storage.save()
+    return jsonify(mod_place.to_dict()), 200
