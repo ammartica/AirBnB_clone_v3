d7681bbea36cb6fa36c275743089388b0b48e4ad
ammartica <amisadaymartinez@gmail.com>
2022-05-24 23:22:20 +0000

    removed validation methods

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,74 +8,54 @@ from api.v1.views import app_views
 from api.v1.app import *
 
 
-def validate_city(city_id):
-    """ validate if query has id to reference """
-    try:
-        valid = storage.get(City, city_id)
-        valid.to_dict()
-    except Exception:
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
+def all_places(city_id):
+    """method retieves all places"""
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
-    return valid
+    places = []
+    for place in city.places:
+        places.append(place.to_dict())
+    return jsonify(places)
 
 
-def validate_place(place_id):
-    """ validate if place obj exists """
-    try:
-        valid = storage.get(Place, place_id)
-        valid.to_dict()
-    except Exception:
-        abort(404)
-    return valid
-
-
-@app_views.route('/cities/<city_id>/places', methods=['GET'],
-                 strict_slashes=False)
-def get_all_places(city_id):
-    """ get all places """
-    if (city_id is not None):
-        city = validate_city(city_id)
-    places_all = []
-    for place in city.places():
-        places_all.append(place.to_dict())
-    return jsonify(places_all)
-
-
-@app_views.route('/places/<place_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_a_place(place_id):
-    """gets one specified place"""
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def one_place(place_id):
+    """method that retrieves a place by id"""
     place = storage.get(Place, place_id)
-    if place is None:
+    if place is not None:
+        return jsonify(my_place.to_dict())
+    else:
         abort(404)
-    return jsonify(place.to_dict())
 
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
-    """ delete place request """
-    place = validate_place(place_id)
+    """method that deletes a place by id"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
     storage.delete(place)
     storage.save()
-    response = {}
-    return jsonify(response), 200
+    return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
 def create_place(city_id):
-    """ create place """
-    if (city_id is not None):
-        city = validate_city(city_id)
-    request_json = request.get_json()
-    if request_json is None:
+    """method to create a new place"""
+    place = request.get_json()
+    if not place:
         abort(400, 'Not a JSON')
-    if 'user_id' not in request_json:
+    if 'user_id' not in place:
         abort(400, 'Missing user_id')
-    if 'name' not in request_json:
+    if 'name' not in place:
         abort(400, 'Missing name')
-    place = storage.get(City, city_id)
-    if place is None:
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
 
     user = storage.get(User, request.json['user_id'])
@@ -90,17 +70,18 @@ def create_place(city_id):
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def update_place(place):
-    """ update place """
+def update_place(place_id):
+    """method to update a place by id"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-
-    request_json = request.get_json()
-    if request_json is None:
+    request = request.get_json()
+    if not request:
         abort(400, 'Not a JSON')
-    for key, value in request_json.items():
-        if (key not in ('id', 'created_at', 'updated_at', 'city_id')):
-            setattr(place, key, value)
-        storage.save()
-        return jsonify(place.to_dict()), 200
+    request['id'] = place.id
+    request['user_id'] = place.user_id
+    request['city_id'] = place.city_id
+    request['created_at'] = place.created_at
+    place.__init__(**request)
+    place.save()
+    return jsonify(place.to_dict()), 200
